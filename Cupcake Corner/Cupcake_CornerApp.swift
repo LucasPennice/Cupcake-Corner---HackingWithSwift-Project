4f7ac3a3615a96c1a6ac85e393368b8e1af31cfc
Lucas Pennice <lucaspennice@gmail.com>
2024-02-16 20:08:45 -0300

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Lucas Pennice on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
